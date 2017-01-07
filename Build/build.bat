d3ecdceb872c585586fd977c8e0de3212039d7b9
Cain O'Sullivan <cain.osullivan@gmail.com>
2017-01-07 14:25:31 +0800

    slight modifications

--- Build/build.bat
+++ Build/build.bat
@@ -1,18 +1,10 @@
 @echo off
 pushd %~dp0
 
-REM set ShouldPublish=""
-REM set NugetKey=""
-REM if "%1"=="publish" set ShouldPublish="nuget"
-REM if not "%2"=="" set NugetKey=%2
-
-REM if %ShouldPublish%=="nuget" echo Publish to NuGet enabled
-
 if exist Build goto Build
 mkdir Build
 
 :Build
-REM %SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Build.proj /m /nr:false /v:M /fl /p:ShouldPublish=%ShouldPublish%;NugetKey=%NugetKey%
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" Build.proj /m /nr:false /v:M /fl /p:SignAssembly=true;AssemblyOriginatorKeyFile=C:\Dropbox\Documents\Cain\Programming\SmtpServer\SmtpServer.pfx
 if errorlevel 1 goto BuildFail
 goto BuildSuccess
