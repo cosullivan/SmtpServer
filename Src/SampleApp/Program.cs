cdc99ad5f1d486fd99aea011c57bd253f4b25a5d
Cain O'Sullivan <cain.osullivan@gmail.com>
2017-05-08 23:21:30 +0800

    removed experimental features

--- Src/SampleApp/Program.cs
+++ Src/SampleApp/Program.cs
@@ -1,68 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Mail;
-using System.Net.Mime;
 using System.Net.Security;
-using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using SmtpServer;
 using SmtpServer.Tracing;
-using MailKit.Net.Smtp;
 using MimeKit;
 using MimeKit.Text;
-using SmtpServer.IO;
-using SmtpServer.Mime;
-using SmtpServer.Text;
-using ContentType = SmtpServer.Mime.ContentType;
 using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 
 namespace SampleApp
 {
-    // https://tools.ietf.org/html/rfc2045
     class Program
     {
-        static string MIME = @"MIME-Version: 1.0
-Content-Type: multipart/mixed; boundary=frontier
-
-This is a message with multiple parts in MIME format.
---frontier
-Content-Type: text/plain
-
-This is the body of the message.
---frontier
-Content-Type: application/octet-stream
-Content-Transfer-Encoding: base64
-
-PGh0bWw+CiAgPGhlYWQ+CiAgPC9oZWFkPgogIDxib2R5PgogICAgPHA+VGhpcyBpcyB0aGUg
-Ym9keSBvZiB0aGUgbWVzc2FnZS48L3A+CiAgPC9ib2R5Pgo8L2h0bWw+Cg==
---frontier--";
-
-        
         static void Main(string[] args)
         {
-            ////var mimeMessageReader = new MimeMessageReader(File.OpenRead(@"C:\Dev\temp\msg.txt"));
-            //var mimeMessageSerializer = new MimeMessageSerializer();
-            ////var mimeMessageReader = new MimeMessageReader(new MemoryStream(Encoding.ASCII.GetBytes(MIME)));
-            //try
-            //{
-            //    //File.OpenRead(@"C:\Dev\Enron Corpus\maildir\allen-p\inbox\31_")
-            //    var mimeMessage = mimeMessageSerializer.DeserializeAsync(File.OpenRead(@"C:\Dev\Enron Corpus\maildir\allen-p\inbox\31_")).Result;
-            //    Console.WriteLine(mimeMessage);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
-            //    throw;
-            //}
-
-
             var cancellationTokenSource = new CancellationTokenSource();
 
             var certificate = CreateCertificate();
