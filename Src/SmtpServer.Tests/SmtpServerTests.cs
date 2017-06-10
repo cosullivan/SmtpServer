21e324c2bc1243daa0aa355b68b92f28c0035782
Cain O'Sullivan <cain.osullivan@gmail.com>
2017-06-10 12:09:14 +0800

    added the CanAuthenticateUser test

--- Src/SmtpServer.Tests/SmtpServerTests.cs
+++ Src/SmtpServer.Tests/SmtpServerTests.cs
@@ -6,6 +6,7 @@ using SmtpServer.Tests.Mocks;
 using Xunit;
 using MailKit.Net.Smtp;
 using MimeKit;
+using SmtpServer.Authentication;
 
 namespace SmtpServer.Tests
 {
@@ -82,6 +83,50 @@ namespace SmtpServer.Tests
             Wait(smtpServerTask);
         }
 
+        [Fact]
+        public void CanAuthenticateUser()
+        {
+            // arrange
+            string user = null;
+            string password = null;
+            var userAuthenticator = new DelegatingUserAuthenticator((u, p) =>
+            {
+                user = u;
+                password = p;
+
+                return true;
+            });
+
+            var options = _optionsBuilder
+                .AllowUnsecureAuthentication()
+                .UserAuthenticator(userAuthenticator);
+
+            var smtpServer = new SmtpServer(options.Build());
+            var smtpClient = new SmtpClient();
+            var smtpServerTask = smtpServer.StartAsync(_cancellationTokenSource.Token);
+
+            var message = new MimeKit.MimeMessage();
+            message.From.Add(new MailboxAddress("test1@test.com"));
+            message.To.Add(new MailboxAddress("test2@test.com"));
+            message.Subject = "Assunto teste acento çãõáéíóú";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Assunto teste acento çãõáéíóú"
+            };
+
+            // act
+            smtpClient.Connect("localhost", 25, false);
+            smtpClient.Authenticate("user", "password");
+            smtpClient.Send(message);
+
+            // assert
+            Assert.Equal(1, _messageStore.Messages.Count);
+            Assert.Equal("user", user);
+            Assert.Equal("password", password);
+
+            Wait(smtpServerTask);
+        }
+
         void Wait(Task smtpServerTask)
         {
             _cancellationTokenSource.Cancel();
@@ -94,6 +139,37 @@ namespace SmtpServer.Tests
             {
                 e.Handle(exception => exception is OperationCanceledException);
             }
+        }
+    }
+
+    public sealed class DelegatingUserAuthenticator : UserAuthenticator
+    {
+        readonly Func<string, string, bool> _delegate;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="delegate">THe delegate to execute for the authentication.</param>
+        public DelegatingUserAuthenticator(Func<string, string, bool> @delegate)
+        {
+            _delegate = @delegate;
+        }
+
+        /// <summary>
+        /// Authenticate a user account.
+        /// </summary>
+        /// <param name="context">The session context.</param>
+        /// <param name="user">The user to authenticate.</param>
+        /// <param name="password">The password of the user.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>true if the user is authenticated, false if not.</returns>
+        public override Task<bool> AuthenticateAsync(
+            ISessionContext context,
+            string user,
+            string password,
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_delegate(user, password));
         }
     }
 }
