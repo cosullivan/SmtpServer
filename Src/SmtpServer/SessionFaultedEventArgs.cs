5239a3bdb7fd0ef4a4890b51fd45b1d3394f6376
Jared Sowers <jared.sowers@zebit.com>
2019-06-14 11:49:30 -0700

    Allow Exceptions in the session to be bubbled up by raising a SessionFaulted event.

---
+++ Src/SmtpServer/SessionFaultedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmtpServer
+{
+    public class SessionFaultedEventArgs : SessionEventArgs
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="context">The session context.</param>
+        /// <param name="exception">The exception that occured</param>
+        public SessionFaultedEventArgs(ISessionContext context, Exception exception) : base(context)
+        {
+            Exception = exception;
+        }
+
+        /// <summary>
+        /// Returns the exception.
+        /// </summary>
+        public Exception Exception { get; }
+    }
+}
