86972e7abd97aa7e7f3151b703e59aad18a34a42
Tino Hager <tino.hager@nager.at>
2024-08-30 11:26:14 +0200

    namespace cleanup

--- Src/SmtpServer.Tests/RawSmtpClient.cs
+++ Src/SmtpServer.Tests/RawSmtpClient.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
--- Src/SmtpServer.Tests/SmtpServerTests.cs
+++ Src/SmtpServer.Tests/SmtpServerTests.cs
@@ -10,7 +10,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
