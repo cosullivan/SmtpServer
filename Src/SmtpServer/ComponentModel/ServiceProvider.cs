cb61a8725db4c4c713ec8a418bcb68366d691d05
Tino Hager <tino.hager@nager.at>
2024-09-16 15:14:33 +0200

    Update ServiceProvider.cs

--- Src/SmtpServer/ComponentModel/ServiceProvider.cs
+++ Src/SmtpServer/ComponentModel/ServiceProvider.cs
@@ -11,7 +11,10 @@ namespace SmtpServer.ComponentModel
     /// </summary>
     public sealed class ServiceProvider : IServiceProvider
     {
-        internal static readonly IServiceProvider Default = new ServiceProvider();
+        /// <summary>
+        /// Default Service Provider
+        /// </summary>
+        public static readonly IServiceProvider Default = new ServiceProvider();
 
         IEndpointListenerFactory _endpointListenerFactory;
         IUserAuthenticatorFactory _userAuthenticatorFactory;
