36856c4f8ae005f136fcbff9e6c6ef2c9f8355b4
Georg Jung <git@gjung.com>
2025-02-26 16:44:39 +0100

    Add `IServiceScope` support

---
+++ Src/SmtpServer/ISessionServiceProviderFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SmtpServer
+{
+    /// <summary>
+    /// Allows customization of the IServiceProvider instance that is used in the session context.
+    /// </summary>
+    public interface ISessionServiceProviderFactory
+    {
+        /// <summary>
+        /// Creates an IServiceProvider instance for one session context.
+        /// </summary>
+        IServiceProvider CreateServiceProvider(IServiceProvider rootServiceProvider);
+    }
+}
