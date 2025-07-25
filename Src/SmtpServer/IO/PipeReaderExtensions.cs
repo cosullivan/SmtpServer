6b1e502b1ffd296d5f633f8d00e2c7f44c8f711e
Tino Hager <tino.hager@nager.at>
2025-07-25 23:15:49 +0200

    cleanup params

--- Src/SmtpServer/IO/PipeReaderExtensions.cs
+++ Src/SmtpServer/IO/PipeReaderExtensions.cs
@@ -22,7 +22,7 @@ namespace SmtpServer.IO
         /// <param name="reader">The reader to read from.</param>
         /// <param name="sequence">The sequence to find to terminate the read operation.</param>
         /// <param name="func">The callback to execute to process the buffer.</param>
-        /// <param name="maxMessageSizeOptions"> Handling of MaxMessageSize</param>
+        /// <param name="maxMessageSizeOptions">Handling of MaxMessageSize.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The value that was read from the buffer.</returns>
         static async ValueTask ReadUntilAsync(PipeReader reader, byte[] sequence, Func<ReadOnlySequence<byte>, Task> func, IMaxMessageSizeOptions maxMessageSizeOptions, CancellationToken cancellationToken)
@@ -67,7 +67,7 @@ namespace SmtpServer.IO
         /// </summary>
         /// <param name="reader">The reader to read from.</param>
         /// <param name="func">The action to process the buffer.</param>
-        /// <param name="maxMessageSizeOptions"> Handling of MaxMessageSize</param>
+        /// <param name="maxMessageSizeOptions">Handling of MaxMessageSize.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A task that can be used to wait on the operation on complete.</returns>
         internal static ValueTask ReadLineAsync(this PipeReader reader, Func<ReadOnlySequence<byte>, Task> func, IMaxMessageSizeOptions maxMessageSizeOptions, CancellationToken cancellationToken = default)
@@ -84,7 +84,7 @@ namespace SmtpServer.IO
         /// Reads a line from the reader.
         /// </summary>
         /// <param name="reader">The reader to read from.</param>
-        /// <param name="maxMessageSizeOptions"> Handling of MaxMessageSize</param>
+        /// <param name="maxMessageSizeOptions">Handling of MaxMessageSize.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>A task that can be used to wait on the operation on complete.</returns>
         internal static ValueTask<string> ReadLineAsync(this PipeReader reader, IMaxMessageSizeOptions maxMessageSizeOptions, CancellationToken cancellationToken = default)
@@ -132,7 +132,7 @@ namespace SmtpServer.IO
         /// </summary>
         /// <param name="reader">The reader to read from.</param>
         /// <param name="func">The action to process the buffer.</param>
-        /// <param name="maxMessageSizeOptions"> Handling of MaxMessageSize</param>
+        /// <param name="maxMessageSizeOptions">Handling of MaxMessageSize.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The value that was read from the buffer.</returns>
         internal static async ValueTask ReadDotBlockAsync(this PipeReader reader, Func<ReadOnlySequence<byte>, Task> func, IMaxMessageSizeOptions maxMessageSizeOptions, CancellationToken cancellationToken = default)
